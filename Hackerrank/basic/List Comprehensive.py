793dce305e0c6a99bf257b9c0f84759ca78c2f10
Vasanth N <vasanthnimmala69@outlook.com>
2022-08-27 16:14:09 +0530

    Python List Comprehensive

--- Hackerrank/basic/List Comprehensive.py
+++ Hackerrank/basic/List Comprehensive.py
@@ -1,10 +1,11 @@
-#x,y,z,n=1,1,2,3
+# x,y,z,n=3,2,3,2
+
 x=4
 y=5
 z=6
 n=-10
-# pushing sublists into list a if condition satisfies
-a=[[i,j,k] for i in range(n+1) for j in range(n+1) for k in range(n+1) if 0<=i<=x and 0<=j<=y and 0<=k<=z]
+
+# pushing sublists into list a if condition satisfies and also checking each sublist is equal to n or not
+# loop from 0 to x+1 only
+a=[[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if i+j+k != n]
 print(a)
-b=[k for i,k in enumerate(a) if sum(k) != n]
-print(b)
