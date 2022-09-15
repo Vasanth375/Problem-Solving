760dc204f59d7653e86bce8d53f5da0199c9ac56
Vasanth N <vasanthnimmala69@outlook.com>
2022-09-15 22:30:41 +0530

    DSA Python

--- Hackerrank/basic/sales of match.py
+++ Hackerrank/basic/sales of match.py
@@ -3,27 +3,20 @@
 2) in the list count from 0 if two elements are same then increment k value by 1 and remove 
     remaining same equal elements where in the list.
 '''
-#list declaration
-ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
-m,k=0,0
-n=len(ar)
-#remove specified elements in ar list
-def remove_el(m,n):
-    ar.pop(n)
-    ar.pop(m)
+# run code
 
-for i in range(n):
-    for j in range(1,n):
-        if i!=j:
-            if ar[i]==ar[j]:
-                k+=1
-                remove_el(i,j)
-                break
-    n=len(ar)
-# for i in range(n):
-#     for j in range(i+1,n):
-#         if ar[i]==ar[j]:    
-#             k+=1
-#             remove_el(ar[j],ar[i])
-#             break
-#     n=len(ar)
+ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
+# ar = [1,2,1,2,1,3,2]
+countDict = dict()
+
+for i in ar:
+    countDict[i] = ar.count(i)
+
+finalList = []
+for i in countDict:
+    for j in range(countDict[i]+1):
+        if j % 2 == 0:
+            if j != 0:
+                finalList.append([j])
+
+print(len(finalList))
