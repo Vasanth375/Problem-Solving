126553a5f32982ae279131dacc7235e77cb33475
Vasanth N <vasanthnimmala69@outlook.com>
2023-01-23 22:12:05 +0530

    Hackerrank

--- Hackerrank/basic/MigratoryBirds.py
+++ Hackerrank/basic/MigratoryBirds.py
@@ -1,11 +1,18 @@
 arr = [1,4,4,4,5,3]
-arr = [1,1,2,2,3]
+arr = [1 ,2 ,3 ,4 ,5 ,4 ,3 ,2 ,1 ,3 ,4]
+arr = [2 ,4 ,3 ,2 ,3 ,1 ,2 ,1 ,3 ,3]
 highBird = max(arr)
 countBirds = dict()
-for i in range(1,highBird+1):
+for i in range(1,6):
     countBirds[i] = arr.count(i)
-#print(countBirds)
 
-maxFrequency = list(countBirds.keys())
 values = list(countBirds.values())
-print(maxFrequency[max(values)])
+
+maxValue = max(values)
+l = dict()
+for i in range(1, 6):
+    if countBirds[i] == maxValue:
+        l[i] = countBirds[i]
+
+for i in l:
+    print(i)
