fddf07f31657039fbc5704278df7da1f24a8b0c3
Vasanth N <vasanthnimmala69@outlook.com>
2023-01-23 22:20:05 +0530

    Hackerrank

--- Hackerrank/basic/Number of lines.py
+++ Hackerrank/basic/Number of lines.py
@@ -1,21 +1,14 @@
+#Accepted
+
+def NoLines(x1,v1, x2,v2):
+    for i in range(1,10000):
+        '''Loop through from 1 to 10000 and check the condition
+        return YES if condition is true or NO'''
+        x1 += v1   
+        x2 += v2
+        if x1 == x2:
+            return('YES')
+    return('NO')
+
 x1,v1,x2,v2=2,1,1,2
-#x1, v1, x2, v2 = 1, 8 , 2, 5
-# 1. 8 testcases FAILED
-# 2. 5 testcases FAILED
-if x1>1000:
-    print("YES")
-else:
-    k1=[i for i in range(x1,(x1+v1),v1)]
-    # print(k1)
-    k2=[i for i in range(x2,(x2+v2),v2)]
-    # print(k2)
-    n=0
-    for i,v in enumerate(k1):
-        for j,m in enumerate(k2):
-            if v == m and i == j:
-                print(v,m,i,j)
-                print("YES")
-                n=n+1
-                break
-    if n==0:
-        print("NO")
+print(NoLines(x1,v2, x2,v2))
