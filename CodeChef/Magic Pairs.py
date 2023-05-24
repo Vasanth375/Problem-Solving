15bef8d3bf4901159dda65ca509054ae80eab715
Vasanth N <vasanthnimmala69@outlook.com>
2023-05-24 20:31:02 +0530

    CodeChef

---
+++ CodeChef/Magic Pairs.py
@@ -0,0 +1,8 @@
+## here we are using combination formulae
+# nCr = n!/r!(n-r)!
+# combinations will return an count of pairs with the given number of elements without repeating of values
+
+import math
+
+n = 2
+print(math.comb(n, 2))
