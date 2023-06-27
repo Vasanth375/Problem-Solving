76f97e32613f04ba1ce2b887e470c41bdab4a1db
Vasanth N <vasanthnimmala69@outlook.com>
2023-06-27 21:46:14 +0530

    Leetcode

---
+++ LeetCode/JS/Array Prototype Last.js
@@ -0,0 +1,12 @@
+let arr = [1, 2, 3];
+arr = [];
+
+Array.prototype.last = function () {
+  if (this.length == 0) {
+    return -1;
+  } else {
+    return this[this.length - 1];
+  }
+};
+
+console.log(arr.last());
