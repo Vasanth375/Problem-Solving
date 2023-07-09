c2a4be3378324ad487c569628410d3dcc8480781
Vasanth N <vasanthnimmala69@outlook.com>
2023-07-09 11:37:18 +0530

    LeetCode Medium

---
+++ LeetCode/Reverse Words in a String.py
@@ -0,0 +1,17 @@
+s = "the sky is blue"
+#s = "  hello world  "
+#s = "a        good       example"
+
+s = s.strip(" ")
+
+s = s.split(" ")
+
+i = 0
+while i < len(s):
+    if s[i] == "":
+        s.remove(s[i])
+        i -=1
+    i += 1
+# print(s)
+
+print(" ".join(s[::-1]))
