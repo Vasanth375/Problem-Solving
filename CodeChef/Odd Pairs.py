8d0104350d4f7ec2c6d3e6e85fd122ac4a2a84b9
Vasanth N <vasanthnimmala69@outlook.com>
2023-07-26 23:23:05 +0530

    CodeChef

---
+++ CodeChef/Odd Pairs.py
@@ -0,0 +1,7 @@
+n = 199
+
+# this formula calculates the sum of all odd numbers from 1 to n without explicitly adding them one by one.
+# It is an optimized approach to compute the result in constant time, regardless of the value of n. In your case,
+# it will give the sum of all odd numbers from 1 to 199.
+count = (n * (n)) // 2
+print(count)
