5354fc8ef95890322fe371ae8ff2ff53edfebca4
Vasanth N <vasanthnimmala69@outlook.com>
2023-10-27 20:14:00 +0530

    LeetCode

--- LeetCode/Longest Palindromic Subsequence.py
+++ LeetCode/Longest Palindromic Subsequence.py
@@ -12,5 +12,5 @@ for i in range(n-1, -1, -1):
         if s[i] == s[j]:
             dp[i][j] = dp[i+1][j-1] +2
         else:
-            dp[i][j] = max(dp[i+1][j], dp[i][j-1])
-print(dp[0][n-1])
+            dp[i][j] = max(dp[i+1][j], dp[i][j-1]) # type: ignore
+print(dp[0][n-1])   
