9370c54445b73db2de50a1f1971696f51d8b25af
Vasanth N <vasanthnimmala69@outlook.com>
2023-12-25 13:08:03 +0530

    Java

---
+++ Hackerrank/Java/Java_BigInteger.java
@@ -0,0 +1,17 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * Java_BigInteger
+ */
+public class Java_BigInteger {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger a = sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+        BigInteger add = a.add(b);
+        BigInteger mul = a.multiply(b);
+        System.out.println(add + "\n" + mul);
+    }
+}
