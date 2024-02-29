30f5344178995c557b3e1048197f02bf9a09b561
Vasanth N <vasanthnimmala69@outlook.com>
2024-02-29 20:15:19 +0530

    LeetCode

--- LeetCode/Sort Characters By Frequency.py
+++ LeetCode/Sort Characters By Frequency.py
@@ -1,10 +1,12 @@
-# Time Limit Exceeded
-# 31 / 33 testcases passed
+## I've found Three Approaches for this problem
+# 1. Normal Dictionary
+# 2. Bucket sorting Method using dictionary
+# 3. find all the occurances of a character and store it in a list
+#   return new string based on the descending order of the list item
+
+from collections import Counter, defaultdict
 
 s = "tree"
-s = "raaeaedere"
-s = "cccaaa"
-s = "Aabb"
 newList = []
 valuesList = []
 ss = list(set(s))
@@ -18,12 +20,33 @@ for i in range(len(ss)):
 
 sortedVal = valuesList.copy()
 sortedVal.sort(reverse=True)
-# print(valuesList)
-# print(sortedVal)
 
 newS = ""
 for i in sortedVal:
     k = valuesList.index(i)
     valuesList[k] = -1
     newS += "".join(newList[k])
-print(newS)
+
+
+# Approach: 
+# sort our hashmap in reverse order
+freq = Counter(s)
+
+# for sorting there are 3 parameters essentialy 
+freq = sorted(freq.items(), key = lambda  x: x[1], reverse = True)
+
+ans = ""
+
+for key, value in freq:
+    ans += key*value
+
+## Through Bucket Sorting Approach
+counter = Counter(s)
+buckets = defaultdict(list)
+for char, num in counter.items():
+    buckets[num].append(char)
+
+res = ""
+for i in range(len(s), 0, -1):
+    for c in buckets[i]:
+        res += c * i
