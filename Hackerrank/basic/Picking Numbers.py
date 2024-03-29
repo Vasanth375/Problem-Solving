a425b77fd5e7ed333a13602857275869a9656c7d
Vasanth N <vasanthnimmala69@outlook.com>
2024-03-29 16:51:11 +0530

    Leetcode

---
+++ Hackerrank/basic/Picking Numbers.py
@@ -0,0 +1,26 @@
+#
+# Given an array of integers nums and an integer k, return the number of contiguous
+# subarrays where the product of all the elements in the subarray is strictly less than k.
+#
+a = [4, 6, 5, 3, 3, 1]
+
+nums = [10, 5, 2, 6]
+k = 100
+
+total = 0
+running = 1
+left = 0
+# USING SLIDING WINDOW
+for right in range(len(nums)):
+    # calculating product of current right value
+    running *= nums[right]
+
+    #sliding to the right side and removing the left value from the subarray product
+    while left < right and running >= k:
+        running //= nums[left]
+        left += 1
+
+    # our case is if product less than k then count the subarray
+    if running < k:
+        total += right - left + 1
+print(total)
